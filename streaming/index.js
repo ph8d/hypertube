3e42e6aa6336c7237df5f951d470a7525ffa2ebc
root <rtarasen@student.unit.ua>
2018-12-11 18:03:06 +0000

    node api origin fix

--- streaming/index.js
+++ streaming/index.js
@@ -24,7 +24,7 @@ app.use(cookieParser());
 app.use((req, res, next) => {
 	res.set({
 		'Access-Control-Allow-Credentials': 'true',
-		'Access-Control-Allow-Origin': 'http://68.183.221.34'
+		'Access-Control-Allow-Origin': 'http://rtarasen.ml'
 	});
 	next();
 });
