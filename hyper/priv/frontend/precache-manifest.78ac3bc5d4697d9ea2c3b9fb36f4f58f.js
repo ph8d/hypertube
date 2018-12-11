b0adf804e42e42156dfabf2527172ae247e77ddb
Roman TARASENKO <rtarasen@student.unit.ua>
2018-12-11 17:49:01 +0200

    new build

---
+++ hyper/priv/frontend/precache-manifest.78ac3bc5d4697d9ea2c3b9fb36f4f58f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f7f419f848288fb271dbdc85d381bd2a",
+    "url": "/static/media/not_found.f7f419f8.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "23d41b9c919ea1f9d2b8",
+    "url": "/static/js/main.23d41b9c.chunk.js"
+  },
+  {
+    "revision": "7fc1d155915221aab663",
+    "url": "/static/js/1.7fc1d155.chunk.js"
+  },
+  {
+    "revision": "23d41b9c919ea1f9d2b8",
+    "url": "/static/css/main.df7b42f4.chunk.css"
+  },
+  {
+    "revision": "28f69d1677af768d4af1c108862a437a",
+    "url": "/index.html"
+  }
+];
--- hyper/priv/frontend/service-worker.js
+++ hyper/priv/frontend/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2c999e99f6e9e186d5dfcc5bfe9ca5a0.js"
+  "/precache-manifest.78ac3bc5d4697d9ea2c3b9fb36f4f58f.js"
 );
 
 workbox.clientsClaim();
